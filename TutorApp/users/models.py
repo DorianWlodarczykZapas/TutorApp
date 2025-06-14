7115175935a667c8f4825e074255adc7ac3b62af
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-06-14 21:41:02 +0200

    build: Basic implementation of user model

--- TutorApp/users/models.py
+++ TutorApp/users/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class User(AbstractUser):
+    SCHOOL_TYPE_CHOICES = [
+        ('PRIMARY', _('Primary School')),
+        ('SECONDARY', _('Secondary School')),
+    ]
+
+    school_type = models.CharField(
+        max_length=10,
+        choices=SCHOOL_TYPE_CHOICES,
+        null=True,
+        blank=True
+    )
+
