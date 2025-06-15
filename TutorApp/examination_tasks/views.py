41e9e140607919378eac5617ec98df930cd4d000
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-06-15 08:27:31 +0200

    fix: Removed unnessesary admin view

--- TutorApp/examination_tasks/views.py
+++ TutorApp/examination_tasks/views.py
@@ -1,34 +1,26 @@
-from typing import Any, Dict
-
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
-from django.views.generic import CreateView
-from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
-from django.contrib import messages
+from django.views.generic import CreateView
 
-from .models import Exam
 from .forms import ExamForm
+from .models import Exam
 
-class AdminRequiredMixin(UserPassesTestMixin):
-    def test_func(self) -> bool:
-        user = self.request.user
-        return bool(user.is_active and user.is_staff)
 
-class ExamCreateView(LoginRequiredMixin, AdminRequiredMixin, CreateView):
+class ExamCreateView(LoginRequiredMixin, CreateView):
     model = Exam
     form_class = ExamForm
-    template_name = 'examination_tasks/exam_form.html'
+    template_name = "examination_tasks/exam_form.html"
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
-        data['title'] = _('Add New Exam')
+        data["title"] = _("Add New Exam")
         return data
 
     def form_valid(self, form):
 
-        response = super().form_valid(form)
+        messages.success(self.request, _("Exam added successfully!"))
 
-        messages.success(self.request, _('Exam added successfully!'))
-
-        return HttpResponseRedirect(reverse_lazy('examination_tasks:exam_add'))
+        return HttpResponseRedirect(reverse_lazy("examination_tasks:exam_add"))
