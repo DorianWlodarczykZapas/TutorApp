618518a3a2a9ccccae660e391c0dc3fd0209a3e6
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-06-17 21:31:38 +0200

    feat: Implementation of add exam task form

--- TutorApp/examination_tasks/forms.py
+++ TutorApp/examination_tasks/forms.py
@@ -1,23 +1,55 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
-from .models import Exam
+
+from .models import Exam, MathMatriculationTasks
+
 
 class ExamForm(forms.ModelForm):
     class Meta:
         model = Exam
         fields = [
-            'year',
-            'month',
-            'tasks_link',
-            'solutions_link',
-            'tasks_count',
-            'level_type',
+            "year",
+            "month",
+            "tasks_link",
+            "solutions_link",
+            "tasks_count",
+            "level_type",
         ]
         labels = {
-            'year': _('Year'),
-            'month': _('Month'),
-            'tasks_link': _('Tasks link'),
-            'solutions_link': _('Solutions link'),
-            'tasks_count': _('Number of tasks'),
-            'level_type': _('Exam level'),
+            "year": _("Year"),
+            "month": _("Month"),
+            "tasks_link": _("Tasks link"),
+            "solutions_link": _("Solutions link"),
+            "tasks_count": _("Number of tasks"),
+            "level_type": _("Exam level"),
         }
+
+
+class AddMatriculationTaskForm(forms.ModelForm):
+    exam = forms.ModelChoiceField(
+        queryset=Exam.objects.all(),
+        label=_("Select Exam"),
+        widget=forms.Select(
+            attrs={
+                "class": "form-control",
+                "placeholder": _("Select exam, e.g. May 2025"),
+            }
+        ),
+    )
+
+    task_id = forms.IntegerField(
+        label=_("Task Number"),
+        widget=forms.NumberInput(
+            attrs={"class": "form-control", "placeholder": _("e.g. 1")}
+        ),
+    )
+
+    category = forms.ChoiceField(
+        choices=MathMatriculationTasks._meta.get_field("category").choices,
+        label=_("Task Category"),
+        widget=forms.Select(attrs={"class": "form-control"}),
+    )
+
+    class Meta:
+        model = MathMatriculationTasks
+        fields = ["exam", "task_id", "category"]
