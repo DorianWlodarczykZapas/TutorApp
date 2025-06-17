7af999748965f7ca2c5d94dc41da5736cedf54f9
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-06-17 22:06:02 +0200

    build: Added missing url about add task to exam

--- TutorApp/examination_tasks/urls.py
+++ TutorApp/examination_tasks/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import ExamCreateView
 
-app_name = 'examination_tasks'
+from .views import AddMatriculationTaskView, ExamCreateView
+
+app_name = "examination_tasks"
 
 urlpatterns = [
-    path('exams/add/', ExamCreateView.as_view(), name='exam_add'),
-
+    path("exams/add/", ExamCreateView.as_view(), name="exam_add"),
+    path(
+        "exams/tasks/add/",
+        AddMatriculationTaskView.as_view(),
+        name="add_matriculation_task",
+    ),
 ]
