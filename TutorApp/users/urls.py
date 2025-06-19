fb160d4e7d7b565d89492e76618da90f80c5b829
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-06-19 18:27:43 +0200

    feat: Added login url into url file

--- TutorApp/users/urls.py
+++ TutorApp/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import UserRegisterView
+from .views import LoginView, UserRegisterView
 
 app_name = "users"
 
 urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
 ]
