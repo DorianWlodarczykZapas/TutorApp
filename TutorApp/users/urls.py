3c7744ae3a0013a0fa6163442c1398c8700ccb20
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-06-19 18:31:28 +0200

    feat:Added logout url into file

--- TutorApp/users/urls.py
+++ TutorApp/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import LoginView, UserRegisterView
+from .views import LoginView, LogoutView, UserRegisterView
 
 app_name = "users"
 
 urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
