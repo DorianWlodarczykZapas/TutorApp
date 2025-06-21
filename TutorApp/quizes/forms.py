da237d71de97bf0d999e37f8b12dbb5592680d99
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-06-21 11:38:50 +0200

    feat: Implementation of Answer form connected to quiz form

--- TutorApp/quizes/forms.py
+++ TutorApp/quizes/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from django.forms import inlineformset_factory
 from django.utils.translation import gettext_lazy as _
 
-from .models import Quiz
+from .models import Answer, Quiz
 
 
 class QuizForm(forms.ModelForm):
@@ -21,3 +22,24 @@ class QuizForm(forms.ModelForm):
             "explanation": _("Explanation"),
             "explanation_picture": _("Explanation image"),
         }
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ["text", "is_correct"]
+        labels = {
+            "text": _("Answer"),
+            "is_correct": _("Correct?"),
+        }
+
+
+AnswerFormSet = inlineformset_factory(
+    Quiz,
+    Answer,
+    form=AnswerForm,
+    extra=4,
+    can_delete=False,
+    min_num=2,
+    validate_min=True,
+)
