cf4c03164c73fc3f0cc6f4c39da40343f2a5348e
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-06-22 20:43:01 +0200

    feat: Implementation of add video form

---
+++ TutorApp/videos/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
+
+from TutorApp.videos.models import Video
+
+
+class AddVideoForm(forms.ModelForm):
+    class Meta:
+        model = Video
+        fields = [
+            "title",
+            "level",
+            "youtube_url",
+            "type",
+            "subcategory",
+        ]
+        labels = {
+            "title": _("Title"),
+            "level": _("Education level"),
+            "youtube_url": _("YouTube link"),
+            "type": _("Book section"),
+            "subcategory": _("Subcategory"),
+        }
+        widgets = {
+            "title": forms.TextInput(
+                attrs={"placeholder": _("Topic title from the book")}
+            ),
+            "youtube_url": forms.URLInput(
+                attrs={"placeholder": _("https://youtube.com/...")}
+            ),
+            "type": forms.Select(choices=Video.section),
+            "subcategory": forms.TextInput(
+                attrs={"placeholder": _("e.g. operations on fractions")}
+            ),
+            "level": forms.Select(choices=Video.VideoLevel.choices),
+        }
