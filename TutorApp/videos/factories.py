8c05639a1772ba506f44b7ff03ec318876d0c0cd
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-06-23 20:46:08 +0200

    Test:Implementation of videos factory

---
+++ TutorApp/videos/factories.py
@@ -0,0 +1,13 @@
+import factory
+from videos.models import Video
+
+
+class VideoFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Video
+
+    title = factory.Faker("sentence", nb_words=4)
+    youtube_url = factory.Faker("url")
+    type = 1
+    subcategory = "Algebra"
+    level = "1"
