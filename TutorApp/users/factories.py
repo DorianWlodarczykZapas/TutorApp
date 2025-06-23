574ea12d0eae676241cb82f6383118777708319e
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-06-23 20:45:06 +0200

    Test: Implementation of user factory

---
+++ TutorApp/users/factories.py
@@ -0,0 +1,18 @@
+import factory
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: f"user{n}")
+    password = factory.PostGenerationMethodCall("set_password", "testpass123")
+    role_type = 1
+    school_type = "PRIMARY"
+
+
+class TeacherFactory(UserFactory):
+    role_type = 2
--- TutorApp/videos/forms.py
+++ TutorApp/videos/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
-from TutorApp.videos.models import Video
+from .models import Video
 
 
 class AddVideoForm(forms.ModelForm):
