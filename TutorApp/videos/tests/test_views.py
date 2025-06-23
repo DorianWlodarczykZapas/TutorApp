9550ab65f6bd3c36f455fe3e8047a5a4119bfe0a
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-06-23 20:52:07 +0200

    Test: Test about correct add video form

--- TutorApp/videos/tests/test_views.py
+++ TutorApp/videos/tests/test_views.py
@@ -4,6 +4,8 @@ from django.test import TestCase
 from django.urls import reverse
 from users.factories import TeacherFactory, UserFactory
 
+from TutorApp.videos.models import Video
+
 
 class VideoCreateViewTests(TestCase):
     def setUp(self):
@@ -34,3 +36,25 @@ class VideoCreateViewTests(TestCase):
     def test_redirect_if_not_logged_in(self):
         response = self.client.get(self.url)
         self.assertRedirects(response, f"/accounts/login/?next={self.url}")
+
+    @patch("videos.views.UserService")
+    def test_valid_video_form_submission_creates_video(self, mock_user_service):
+        """Poprawne dane tworzą wideo i przekierowują"""
+        mock_user_service.return_value.is_teacher.return_value = True
+        self.client.login(username=self.teacher.username, password=self.password)
+
+        data = {
+            "title": "Test Video",
+            "youtube_url": "https://youtube.com/watch?v=xyz123",
+            "type": 4,
+            "subcategory": "Mathematical functions",
+            "level": "2",
+        }
+
+        response = self.client.post(self.url, data)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Video.objects.count(), 1)
+        video = Video.objects.first()
+        self.assertEqual(video.title, "Test Video")
+        self.assertEqual(video.level, "2")
