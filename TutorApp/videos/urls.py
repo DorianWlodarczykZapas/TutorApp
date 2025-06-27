3d05a8d72350bf57293bbf61618f7671b89f7851
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-06-27 23:05:24 +0200

    feat: Added subcategories list view into urls file

--- TutorApp/videos/urls.py
+++ TutorApp/videos/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from .views import SectionListView, VideoCreateView
+from .views import SectionListView, SubcategoryListView, VideoCreateView
 
 app_name = "videos"
 
 urlpatterns = [
     path("add/", VideoCreateView.as_view(), name="add"),
     path("", SectionListView.as_view(), name="section_list"),
+    path(
+        "section/<int:section_id>/",
+        SubcategoryListView.as_view(),
+        name="subcategory_list",
+    ),
 ]
