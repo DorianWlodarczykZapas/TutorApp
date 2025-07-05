94ba67712e86d8785c5f752371912340c8b4cfa1
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-07-05 23:01:52 +0200

    Test: Case that checks multiple filters

--- TutorApp/examination_tasks/tests/test_views.py
+++ TutorApp/examination_tasks/tests/test_views.py
@@ -215,3 +215,13 @@ class SearchMatriculationTaskViewTest(TestCase):
         self.assertIn(self.task2, queryset)
         self.assertNotIn(self.task1, queryset)
         self.assertNotIn(self.task3, queryset)
+
+    def test_get_queryset_with_multiple_filters(self):
+        request = self._create_request({"year": "2020", "category": "1"})
+        self.view.request = request
+
+        queryset = self.view.get_queryset()
+        self.assertEqual(queryset.count(), 1)
+        self.assertIn(self.task1, queryset)
+        self.assertNotIn(self.task2, queryset)
+        self.assertNotIn(self.task3, queryset)
