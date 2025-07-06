b3fb82d8416b03f9c0c7d8b10f4135c086fded51
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-07-06 21:02:59 +0200

    refactor: Removed dipatch method and changes them to TeacherRequiredMixin

--- TutorApp/examination_tasks/views.py
+++ TutorApp/examination_tasks/views.py
@@ -2,29 +2,21 @@ from typing import Any, Dict, Optional
 
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.exceptions import PermissionDenied
-from django.http import HttpResponseForbidden, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView, ListView
-from users.services import UserService
+from users.views import TeacherRequiredMixin
 
 from .forms import AddMatriculationTaskForm, ExamForm
 from .models import Exam, MathMatriculationTasks
 from .services import MatriculationTaskService
 
 
-class ExamCreateView(LoginRequiredMixin, CreateView):
+class ExamCreateView(LoginRequiredMixin, TeacherRequiredMixin, CreateView):
     model = Exam
     form_class = ExamForm
     template_name = "examination_tasks/exam_form.html"
-
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated or request.user.role_type != 2:
-            return HttpResponseForbidden(
-                _("You do not have permission to access this page.")
-            )
-        return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
@@ -37,16 +29,10 @@ class ExamCreateView(LoginRequiredMixin, CreateView):
         return HttpResponseRedirect(reverse_lazy("examination_tasks:exam_add"))
 
 
-class AddMatriculationTaskView(LoginRequiredMixin, CreateView):
+class AddMatriculationTaskView(LoginRequiredMixin, TeacherRequiredMixin, CreateView):
     model = MathMatriculationTasks
     form_class = AddMatriculationTaskForm
     template_name = "tasks/add_matriculation_task.html"
-
-    def dispatch(self, request, *args, **kwargs):
-        user_service = UserService(request.user)
-        if not user_service.is_teacher():
-            raise PermissionDenied(_("Only teachers can add exam tasks."))
-        return super().dispatch(request, *args, **kwargs)
 
     def form_valid(self, form):
         super().form_valid(form)
