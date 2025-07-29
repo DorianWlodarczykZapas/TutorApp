19bd7cf02cfae99e18e768fddee71abc11f06d27
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-07-29 14:38:38 +0200

    feat: Implemenation of method responsible for extracting one task from exam pdf file

--- TutorApp/examination_tasks/services.py
+++ TutorApp/examination_tasks/services.py
@@ -1,3 +1,4 @@
+import typing
 from typing import TYPE_CHECKING, List, Optional
 
 import fitz
@@ -103,3 +104,39 @@ class MatriculationTaskService:
 
         except Exception as e:
             task.content = f"Error while processing PDF: {str(e)}"
+
+    @staticmethod
+    def get_single_task_pdf(task_link: str, pages: list[int]) -> typing.Optional[bytes]:
+        """
+        The function from the given link to the PDF file extracts
+        the pages specified as a list of integers and returns only those pages as bytes.
+
+        :param task_link: Path to the source PDF file.
+        :param pages: A list of page numbers to extract (1-based).
+        :return: A bytes object representing the new PDF, or None if an error occurs or no pages are found.
+        """
+        task = fitz.open()
+        try:
+            with fitz.open(task_link) as exam:
+                for page in pages:
+                    page_index = page - 1
+                    if 0 <= page_index < exam.page_count:
+                        task.insert_pdf(exam, from_page=page_index, to_page=page_index)
+                    else:
+                        print(
+                            f"Warning: Page number {page_index} is not found and going to be missed."
+                        )
+
+                if task.page_count > 0:
+                    pdf_bytes = task.tobytes(garbage=4, deflate=True)
+                    return pdf_bytes
+                else:
+                    return None
+        except FileNotFoundError:
+            print(f"Error: File not found under that path: {task_link}")
+            return None
+        except Exception as e:
+            print(f"An unexpected error has occurred: {e}")
+            return None
+        finally:
+            task.close()
