b28223010a9f15380d7e6fb824e0b54b1a2d59f9
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-10-10 23:48:32 +0200

    feat: Implemantation of method to get pages from pdf

--- TutorApp/examination_tasks/services/extract_task_from_pdf.py
+++ TutorApp/examination_tasks/services/extract_task_from_pdf.py
@@ -1,5 +1,7 @@
 import os
 
+import fitz
+
 
 class ExtractTaskFromPdf:
 
@@ -8,3 +10,10 @@ class ExtractTaskFromPdf:
         """Validates entry parameters."""
         if not os.path.exists(file_path):
             raise FileNotFoundError(f"File not found: {file_path}")
+
+    @staticmethod
+    def _get_page(doc: fitz.Document, page_number: int):
+        """Gets page from pdf."""
+        if page_number < 1 or page_number > doc.page_count:
+            raise ValueError(f"Invalid page number: {page_number}")
+        return doc.load_page(page_number - 1)
