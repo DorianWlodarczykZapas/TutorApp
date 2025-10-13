c25c16629c3c586893cfa938f452e4d21cec746d
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-10-13 17:43:00 +0200

    refactor: Removed get single task method into new method

---
+++ TutorApp/examination_tasks/services/ExtractTaskPagesFromPdf.py
@@ -0,0 +1,42 @@
+import typing
+
+import fitz
+
+
+class ExtractTaskPagesFromPdf:
+    @staticmethod
+    def get_single_task_pdf(task_link: str, pages: list[int]) -> typing.Optional[bytes]:
+        """
+        The function from the given link to the PDF file extracts
+        the pages specified as a list of integers and returns only those pages as bytes.
+        """
+
+        task = fitz.open()
+        try:
+
+            with fitz.open(task_link) as exam:
+
+                for page in pages:
+                    page_index = page - 1
+                    if 0 <= page_index < exam.page_count:
+                        task.insert_pdf(exam, from_page=page_index, to_page=page_index)
+                    else:
+                        print(
+                            f"Warning: Page number {page_index} is not found and going to be missed."
+                        )
+
+                if task.page_count > 0:
+                    pdf_bytes = task.tobytes(garbage=4, deflate=True)
+                    return pdf_bytes
+                else:
+                    return None
+        except FileNotFoundError:
+            print(f"Error: File not found under that path: {task_link}")
+            return None
+        except Exception as e:
+
+            print(f"An unexpected error has occurred inside PyMuPDF: {e}")
+            print(f"Typ błędu: {type(e)}")
+            return None
+        finally:
+            task.close()
