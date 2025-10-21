0ec9929e80d9497ce3760bd115f2a8a97f39f932
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-10-21 14:05:26 +0200

    feat: Implementation of simple add section view

---
+++ TutorApp/examination_tasks/views/section_views.py
@@ -0,0 +1,38 @@
+from typing import Any, Dict
+
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import CreateView
+from users.services import TeacherRequiredMixin
+
+from ..forms import SectionForm
+from ..models import Section
+
+
+class AddSection(LoginRequiredMixin, TeacherRequiredMixin, CreateView):
+    """
+    Simple view that adds section to database via form
+    """
+
+    model = Section
+    form = SectionForm
+    template_name = "examination_tasks/add_section.html"
+    success_url = reverse_lazy("examination_tasks:add_section")
+
+    def get_context_data(self, **kwargs: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        Adds the page title to the template context.
+        """
+        data = super().get_context_data(**kwargs)
+        data["title"] = _("Add New Section")
+        return data
+
+    def form_valid(self, form: SectionForm) -> HttpResponseRedirect:
+        """
+        Method for handling the form for adding examinations to the database
+        """
+        messages.success(self.request, _("Section added successfully!"))
+        return super().form_valid(form)
