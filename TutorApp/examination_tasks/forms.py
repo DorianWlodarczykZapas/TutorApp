c9c18fa843e67b9fdfb20e53bd42846c8b122f81
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-10-22 11:20:58 +0200

    feat: Implementation of training task form

--- TutorApp/examination_tasks/forms.py
+++ TutorApp/examination_tasks/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
-from .models import Book, Exam, ExamTask, Section, Topic
+from .models import Book, Exam, ExamTask, Section, Topic, TrainingTask
 from .services import ExamTaskDBService
 
 
@@ -156,4 +156,28 @@ class TopicForm(forms.ModelForm):
         labels = {
             "section": _("Section Name"),
             "name": _("Specific Topic"),
+        }
+
+
+class TrainingTaskForm(forms.ModelForm):
+    class Meta:
+        model = TrainingTask
+        fields = ["task_content", "answer", "image", "section", "level"]
+        labels = {
+            "task_content": _("Task Content"),
+            "answer": _("Answer"),
+            "image": _("Task Image (optional)"),
+            "section": _("Section (optional)"),
+            "level": _("Difficulty Level"),
+        }
+        widgets = {
+            "task_content": forms.Textarea(
+                attrs={
+                    "rows": 5,
+                    "placeholder": _("Enter the task content here..."),
+                }
+            ),
+            "answer": forms.TextInput(
+                attrs={"placeholder": _("Enter the correct answer...")}
+            ),
         }
