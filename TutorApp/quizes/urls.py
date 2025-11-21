204973198655b715e738290ebe30e6d2f08867e7
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-11-21 22:57:07 +0100

    feat: Added quiz list link to url

--- TutorApp/quizes/urls.py
+++ TutorApp/quizes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views.quiz_views import AddQuiz
+from .views.quiz_views import AddQuiz, QuizList
 
 app_name = "quizes"
 
 urlpatterns = [
     path("add/", AddQuiz.as_view(), name="add_quiz"),
+    path("", QuizList.as_view(), name="quiz_list"),
 ]
