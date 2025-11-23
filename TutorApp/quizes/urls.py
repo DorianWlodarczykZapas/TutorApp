b0599a1d6b16cf6ff6bba9bed57687b6a9b5445e
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-11-23 20:44:54 +0100

    feat: Added url about add question

--- TutorApp/quizes/urls.py
+++ TutorApp/quizes/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from .views.question_views import AddQuestion
 from .views.quiz_views import AddQuiz, QuizList
 
 app_name = "quizes"
@@ -7,4 +8,7 @@ app_name = "quizes"
 urlpatterns = [
     path("add/", AddQuiz.as_view(), name="add_quiz"),
     path("", QuizList.as_view(), name="quiz_list"),
+    path(
+        "quiz/<int:quiz_pk>/add-question/", AddQuestion.as_view(), name="add_question"
+    ),
 ]
