f57053a87d4d4b9d534c090a0b4731d8a4358a7e
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-11-23 20:43:28 +0100

    feat: Implemenetation of form valid method and get success url

--- TutorApp/quizes/views/question_views.py
+++ TutorApp/quizes/views/question_views.py
@@ -1,11 +1,12 @@
 from typing import Any, Dict
 
 from django.http import HttpResponse
+from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from ...users.views import TeacherRequiredMixin
 from ..forms.question_forms import AnswerFormSet, QuestionForm
-from ..models import Question
+from ..models import Question, Quiz
 
 
 class AddQuestion(TeacherRequiredMixin, CreateView):
@@ -23,4 +24,22 @@ class AddQuestion(TeacherRequiredMixin, CreateView):
         return context
 
     def form_valid(self, form: QuestionForm) -> HttpResponse:
-        return super().form_valid(form)
+        formset = AnswerFormSet(self.request.POST)
+
+        if formset.is_valid():
+            self.object = form.save(commit=False)
+            quiz_pk = self.kwargs["quiz_pk"]
+            quiz = Quiz.objects.get(pk=quiz_pk)
+
+            self.object.quiz = quiz
+            self.object.save()
+            formset.instance = self.object
+            formset.save()
+
+            return super().form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def get_success_url(self) -> str:
+        quiz_pk = self.kwargs["quiz_pk"]
+        return reverse_lazy("quizes:add_question", kwargs={"quiz_pk": quiz_pk})
