b9b3cec6a09d699d2713b8f16e1b8428213f2999
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-11-28 22:49:49 +0100

    feat: Implementation of Quiz Step Form

--- TutorApp/quizes/forms/quiz_wizard_forms.py
+++ TutorApp/quizes/forms/quiz_wizard_forms.py
@@ -4,5 +4,9 @@ from django import forms
 class QuizStepForm(forms.Form):
     def __init__(self, question, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        # answers = question.answers.all()
+        answers = question.answers.all()
+        self.fields["selected_answers"] = forms.MultipleChoiceField(
+            choices=[(answer.id, answer.text) for answer in answers],
+            widget=forms.CheckboxSelectMultiple,
+            required=False,
+        )
