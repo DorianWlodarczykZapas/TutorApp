6f1d72b76d0e81b5f5fe4c622bdce9aa73a2ea3c
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-11-28 23:57:17 +0100

    feat: Implementation of quiz start form

--- TutorApp/quizes/views/quiz_solve_views.py
+++ TutorApp/quizes/views/quiz_solve_views.py
@@ -1,9 +1,9 @@
-from django.contrib.auth.middleware import LoginRequiredMiddleware
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from formtools.wizard.views import SessionWizardView
 
 
-class SolveQuizWizard(LoginRequiredMiddleware, SessionWizardView):
+class SolveQuizWizard(LoginRequiredMixin, SessionWizardView):
     template_name = "quizes/quiz_solve_wizard.html"
 
     def get_form_list(self):
