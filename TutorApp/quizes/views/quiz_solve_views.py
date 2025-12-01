7ba6acddfdb1809882ed729b37f7bee371c1d783
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-12-01 22:26:57 +0100

    feat: Implementation of get form list method

--- TutorApp/quizes/views/quiz_solve_views.py
+++ TutorApp/quizes/views/quiz_solve_views.py
@@ -1,4 +1,5 @@
-from typing import Any, Dict
+import logging
+from typing import Any, Dict, OrderedDict
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, HttpResponseRedirect
@@ -7,15 +8,47 @@ from django.urls import reverse
 from django.views.generic import FormView
 from formtools.wizard.views import SessionWizardView
 
-from ..forms.quiz_wizard_forms import QuizStartForm
+from ..forms.quiz_wizard_forms import QuizStartForm, QuizStepForm
 from ..models import Quiz
+
+logger = logging.getLogger(__name__)
 
 
 class SolveQuizWizard(LoginRequiredMixin, SessionWizardView):
     template_name = "quizes/quiz_solve_wizard.html"
 
-    def get_form_list(self):
-        pass
+    def get_form_list(self) -> OrderedDict[str, type]:
+        """
+        Build dynamic form list based on quiz questions.
+        """
+
+        quiz_pk = self.kwargs["quiz_pk"]
+        quiz = get_object_or_404(Quiz, pk=quiz_pk)
+        question_count = self.request.GET.get("question_count", "all")
+        questions = quiz.questions.all()
+
+        available_count = questions.count()
+        if available_count == 0:
+            logger.error(f"Quiz {quiz_pk} has no questions!")
+            raise ValueError(f"Quiz '{quiz.title}' has no questions. Cannot start.")
+
+        questions = questions.order_by("?")
+
+        if question_count != "all":
+            try:
+                question_count = int(question_count)
+            except (ValueError, TypeError):
+                logger.warning(f"Invalid question_count '{question_count}', using 10")
+            question_count = 10
+
+        question_count = min(question_count, available_count)
+        questions = questions[:question_count]
+
+        form_list = []
+        for question in questions:
+            form_list.append((f"question_{question.id}", QuizStepForm))
+
+        return OrderedDict(form_list)
 
     def done(self, form_list, **kwargs) -> HttpResponse:
         pass
