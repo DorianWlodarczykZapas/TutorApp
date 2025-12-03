8f1be51baa101a4d882874e01b5eab825a7191f5
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-12-03 12:10:54 +0100

    feat: Implementation of done method to calculate user result from quiz

--- TutorApp/quizes/views/quiz_solve_views.py
+++ TutorApp/quizes/views/quiz_solve_views.py
@@ -2,14 +2,15 @@ import logging
 from typing import Any, Dict, OrderedDict
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
+from django.utils import timezone
 from django.views.generic import FormView
 from formtools.wizard.views import SessionWizardView
 
 from ..forms.quiz_wizard_forms import QuizStartForm, QuizStepForm
-from ..models import Quiz
+from ..models import Answer, Question, Quiz, QuizAttempt, UserAnswer
 
 logger = logging.getLogger(__name__)
 
@@ -50,24 +51,72 @@ class SolveQuizWizard(LoginRequiredMixin, SessionWizardView):
 
         return OrderedDict(form_list)
 
-    # def done(self, form_list, **kwargs) -> HttpResponse:
-    #     quiz_pk = self.kwargs["quiz_pk"]
-    #     quiz = get_object_or_404(Quiz, pk=quiz_pk)
-    #
-    #     user_answers_data = []
-    #
-    #     total_score = 0
-    #
-    #     max_possible_score = 0
-    #
-    #     for step_name in self.get_form_list().keys():
-    #         step_data = self.get_cleaned_data_for_step(step_name)
-    #
-    #         selected_answers_ids = step_data.get["selected_answers", []]
-    #         question_id = int(step_name.split("_")[1])
-    #         question = get_object_or_404(Question, pk=question_id)
-    #
-    #     return redirect('quizes:quiz_list')
+    def done(self, form_list, **kwargs) -> HttpResponse:
+        quiz_pk = self.kwargs["quiz_pk"]
+        quiz = get_object_or_404(Quiz, pk=quiz_pk)
+
+        user_answers_data = []
+
+        total_score = 0
+
+        max_possible_score = 0
+
+        for step_name in self.get_form_list().keys():
+            step_data = self.get_cleaned_data_for_step(step_name)
+
+            selected_answers_ids = step_data.get("selected_answers", [])
+            question_id = int(step_name.split("_")[1])
+            question = get_object_or_404(Question, pk=question_id)
+
+            all_answers = question.answers.all()
+
+            correct_answers = [a for a in all_answers if a.is_correct]
+            correct_count = len(correct_answers)
+
+            points_per_correct = 1.0 / correct_count if correct_count > 0 else 0
+
+            earned_points = 0
+            for answer_id in selected_answers_ids:
+                answer_id = int(answer_id)
+
+                if any(a.id == answer_id and a.is_correct for a in all_answers):
+                    earned_points += points_per_correct
+
+            earned_points = round(earned_points, 2)
+
+            total_score += earned_points
+
+            max_possible_score += 1.0
+
+            user_answers_data.append(
+                {
+                    "question": question,
+                    "selected_answers_ids": [int(aid) for aid in selected_answers_ids],
+                    "points_earned": earned_points,
+                }
+            )
+
+        quiz_attempt = QuizAttempt.objects.create(
+            user=self.request.user,
+            quiz=quiz,
+            score=total_score,
+            max_score=int(max_possible_score),
+            completed_at=timezone.now(),
+        )
+
+        for data in user_answers_data:
+            user_answer = UserAnswer.objects.create(
+                attempt=quiz_attempt,
+                question=data["question"],
+                points_earned=data["points_earned"],
+            )
+
+            answer_objects = Answer.objects.filter(
+                id__in=data["selected_answers_ids"],
+            )
+            user_answer.selected_answers.set(answer_objects)
+
+        return redirect("quizes:quiz_list")
 
 
 class QuizStartView(LoginRequiredMixin, FormView):
