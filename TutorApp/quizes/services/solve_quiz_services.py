0f76bf7e23d6e0c8614194d5a826c0fcb44be194
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-12-06 23:55:27 +0100

    feat: Basic implementation of service to contain all methods to work with quizes solving and checking views

---
+++ TutorApp/quizes/services/solve_quiz_services.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+class QuizSolveService:
+    """
+    Service that contains methods to get, calculate and pass data about user attempt to quiz
+    """
+
+
+
+
+    def calculate_score(self, questions, user_answers ) -> float:
+
+        pass
