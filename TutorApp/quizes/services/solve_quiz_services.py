1e959af34dd84be08937a6a015bc3dc81caf0b93
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-12-07 22:15:46 +0100

    feat: Added docstring to quiz solve class and calculate score method

--- TutorApp/quizes/services/solve_quiz_services.py
+++ TutorApp/quizes/services/solve_quiz_services.py
@@ -1,16 +1,18 @@
-
-
-
-
-
 class QuizSolveService:
     """
-    Service that contains methods to get, calculate and pass data about user attempt to quiz
+    A class containing methods that handle actions related to solving quizzes.
     """
 
+    def calculate_score(self, questions, user_answers) -> float:
+        """
+        Method for calculating the user's quiz score
+
+        Args:
+            questions (list): A list of the questions
+            user_answers (list): A list of the user answers
 
 
-
-    def calculate_score(self, questions, user_answers ) -> float:
-
+        Returns:
+            float: The user's quiz score
+        """
         pass
