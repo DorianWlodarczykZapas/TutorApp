27c412c655420baffedac1716036a60f9104c4f7
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-12-08 22:32:10 +0100

    feat: Added simple get user answers method with docstring and typing

--- TutorApp/quizes/services/solve_quiz_services.py
+++ TutorApp/quizes/services/solve_quiz_services.py
@@ -1,3 +1,6 @@
+from typing import List, Tuple
+
+
 class QuizSolveService:
     """
     A class containing methods that handle actions related to solving quizzes.
@@ -14,5 +17,20 @@ class QuizSolveService:
 
         Returns:
             float: The user's quiz score
+        """
+        pass
+
+    def get_user_answers(
+        self, form_list: List[Tuple[str, str]]
+    ) -> List[Tuple[str, str]]:
+        """
+        Method for getting the user answers
+
+        Args:
+        form_list (list): A list of the forms from wizard
+
+        Returns:
+        List[Tuple[str, str]]: A list of the user answers [quiz_question_id, user_answer]
+
         """
         pass
