3966381e7bd6cdd54ab78550a4ac81d1cec6ea91
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-12-09 14:04:46 +0100

    refactor: Fixed few docstrings about methods and and typings in quiz solve service class

--- TutorApp/quizes/services/solve_quiz_services.py
+++ TutorApp/quizes/services/solve_quiz_services.py
@@ -1,5 +1,7 @@
 from typing import List, Tuple
 
+from ..models import Question
+
 
 class QuizSolveService:
     """
@@ -11,12 +13,14 @@ class QuizSolveService:
         -method for saving user solutions to the database
     """
 
-    def calculate_score(self, questions, user_answers) -> float:
+    def calculate_score(
+        self, questions: List[Question], user_answers: List[str, List[int]]
+    ) -> float:
         """
         Method for calculating the user's quiz score
 
         Args:
-            questions (list): A list of the questions
+            questions (list): A list of the question objects
             user_answers (list): A list of the user answers
 
 
@@ -32,10 +36,26 @@ class QuizSolveService:
         Method for getting the user answers
 
         Args:
-        form_list (list): A list of the forms from wizard
+        form_list (list): A list of tuples(steps) the forms from wizard
 
         Returns:
         List[Tuple[str, str]]: A list of the user answers [quiz_question_id, user_answer]
 
         """
         pass
+
+    def compare_user_answers(
+        self, question: List[Question], user_answers: List[Tuple[str, str]]
+    ) -> float:
+        """
+        A method that compares the user's answers with the correct answers for a given question.
+
+
+        Args:
+            question (list): A list of the question objects
+            user_answers (list): A list of the user answers
+
+        Returns:
+
+            float: The user's question score
+        """
