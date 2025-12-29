5c778cf2c3cb56ff58b7e9b40cabc005bc084ef7
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-12-29 11:16:27 +0100

    refactor: Changed catalogue to directory

--- TutorApp/examination_tasks/services/tempFileService.py
+++ TutorApp/examination_tasks/services/tempFileService.py
@@ -1,20 +1,20 @@
 class TempFileService:
     """
     Class containing services related to temporary files.
-        -method for creating a temporary catalogue
+        -method for creating a temporary directory
         -method for transferring the temporary file
-        -method for cleaning the catalogue
+        -method for cleaning the directory
         -method for checking if the file exists
         -method for building the final path
     """
 
-    def create_temp_catalogue(self):
+    def create_temp_directory(self):
         pass
 
     def transfer_the_temp_file(self):
         pass
 
-    def clean_the_temp_catalogue(self):
+    def clean_the_temp_directory(self):
         pass
 
     def check_if_file_exists(self):
