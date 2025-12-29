6d5daca5f9fb44318d2a4abc070b52d7977235c2
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-12-29 11:15:19 +0100

    refactor: Basic implementation of TempFileService class

---
+++ TutorApp/examination_tasks/services/tempFileService.py
@@ -0,0 +1,24 @@
+class TempFileService:
+    """
+    Class containing services related to temporary files.
+        -method for creating a temporary catalogue
+        -method for transferring the temporary file
+        -method for cleaning the catalogue
+        -method for checking if the file exists
+        -method for building the final path
+    """
+
+    def create_temp_catalogue(self):
+        pass
+
+    def transfer_the_temp_file(self):
+        pass
+
+    def clean_the_temp_catalogue(self):
+        pass
+
+    def check_if_file_exists(self):
+        pass
+
+    def build_final_path(self):
+        pass
