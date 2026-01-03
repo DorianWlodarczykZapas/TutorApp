d5cdadc3b36b81da1cd4b18a96a7d13368625fcc
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-01-03 13:23:23 +0100

    refactor: Added translations in model motif

--- TutorApp/motifs/models.py
+++ TutorApp/motifs/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-
-from ..examination_tasks.choices import LEVEL_CHOICES, SubjectChoices
-from ..examination_tasks.models import Section
+from django.utils.translation import gettext_lazy as _
+from examination_tasks.choices import LEVEL_CHOICES, SubjectChoices
+from examination_tasks.models import Section
 
 
 def motif_image_path(instance, filename) -> str:
@@ -19,48 +19,52 @@ def motif_image_path(instance, filename) -> str:
 
 class Motif(models.Model):
     subject = models.CharField(
-        max_length=10, choices=SubjectChoices, verbose_name="Subject", default=1
+        max_length=10, choices=SubjectChoices, verbose_name=_("Subject"), default=1
     )
     section = models.ForeignKey(
-        Section, on_delete=models.CASCADE, related_name="motifs", verbose_name="Section"
+        Section,
+        on_delete=models.CASCADE,
+        related_name="motifs",
+        verbose_name=_("Section"),
     )
     level_type = models.IntegerField(
         choices=LEVEL_CHOICES,
         null=True,
         blank=True,
-        help_text="Motif level: basic or extended (Matriculation only)",
+        verbose_name=_("Level"),
+        help_text=_("Motif level: basic or extended (Matriculation only)"),
     )
 
     content = models.CharField(
         max_length=255,
-        verbose_name="Motif content",
-        help_text="Whole motif content",
+        verbose_name=_("Motif content"),
+        help_text=_("Whole motif content"),
     )
 
     answer = models.TextField(
-        verbose_name="answer",
-        help_text="Explanation about chosen motif",
+        verbose_name=_("answer"),
+        help_text=_("Explanation about chosen motif"),
     )
     answer_picture = models.ImageField(
         upload_to=motif_image_path,
         blank=True,
         null=True,
-        help_text="Motif explanation picture if needed",
+        help_text=_("Motif explanation picture if needed"),
     )
     is_in_matriculation_sheets = models.BooleanField(
         default=False,
-        verbose_name="If exists in matriculation sheets",
+        verbose_name=_("If exists in matriculation sheets"),
     )
 
     is_mandatory = models.BooleanField(
         default=True,
-        verbose_name="Mandatory for students",
-        help_text="Whether students must learn this motif",
+        verbose_name=_("Mandatory for students"),
+        help_text=_("Whether students must learn this motif"),
     )
 
     class Meta:
-        verbose_name = "Motif in subject"
-        verbose_name_plural = "Motifs in subject"
+        verbose_name = _("Motif")
+        verbose_name_plural = _("Motifs")
         ordering = ["subject", "section", "level_type"]
 
     def __str__(self):
