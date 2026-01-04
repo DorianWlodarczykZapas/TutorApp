5cc4a2705dab747e8db90797f3f8b7099937311d
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-01-04 21:48:43 +0100

    feat: Implementation of form_valid method in add motif view

---
+++ TutorApp/motifs/views/motif_views.py
@@ -0,0 +1,22 @@
+from django.contrib import messages
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import CreateView
+from users.mixins import TeacherRequiredMixin
+
+from .forms.motif_forms import AddMotifForm
+from .models import Motif
+
+
+class AddMotifView(TeacherRequiredMixin, CreateView):
+    model = Motif
+    form_class = AddMotifForm
+    template_name = "motifs/add_motif.html"
+    success_url = reverse_lazy("motifs:add_motif")
+
+    def form_valid(self, form: AddMotifForm) -> HttpResponse:
+        response = super().form_valid(form)
+        messages.success(self.request, _("Successfully added motif"))
+
+        return response
