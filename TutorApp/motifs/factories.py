d40c834785ce397ec0e8e588a94c97fefadfb913
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-01-05 22:14:21 +0100

    feat: Implementation of motif factory in motifs app

---
+++ TutorApp/motifs/factories.py
@@ -0,0 +1,26 @@
+import factory
+from examination_tasks.tests.factories import SectionFactory
+from factory.django import DjangoModelFactory
+
+from .models import Motif
+
+
+class MotifFactory(DjangoModelFactory):
+
+    class Meta:
+        model = Motif
+
+    subject = factory.Iterator(["math", "physics", "it"])
+
+    section = factory.SubFactory(SectionFactory)
+
+    level_type = factory.Iterator([1, 2, None])
+
+    content = factory.Faker("sentence", nb_words=6, locale="pl_PL")
+
+    answer = factory.Faker("paragraph", nb_sentences=5, locale="pl_PL")
+
+    answer_picture = None
+
+    is_in_matriculation_sheets = False
+    is_mandatory = True
