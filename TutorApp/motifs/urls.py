9fe525ad3c67e8a6ed7029e79f49dd7f35a9b768
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-01-11 09:34:36 +0100

    feat: Added url about delete motif

--- TutorApp/motifs/urls.py
+++ TutorApp/motifs/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views.motif_views import AddMotifView, MotifListView
+from .views.motif_views import AddMotifView, MotifListView, MotifDeleteView
 
 app_name = "motifs"
 
 urlpatterns = [
     path("add/", AddMotifView.as_view(), name="add_motif"),
     path("", MotifListView.as_view(), name="list_motifs"),
+    path("<int:pk>/delete/", MotifDeleteView.as_view(), name="delete_motif"),
 ]
