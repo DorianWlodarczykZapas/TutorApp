4339c2b1b9830677d1ad14988d304975037ced14
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-01-11 09:33:02 +0100

    feat: Implementation of delete view in motifs

--- TutorApp/motifs/views/motif_views.py
+++ TutorApp/motifs/views/motif_views.py
@@ -8,6 +8,7 @@ from django_filters.views import FilterView
 from ..forms.motif_forms import AddMotifForm
 from ..models import Motif
 from ..filters import MotifFilter
+from django.views.generic import DeleteView
 
 class AddMotifView(TeacherRequiredMixin, CreateView):
     model = Motif
@@ -27,3 +28,10 @@ class MotifListView(LoginRequiredMixin, FilterView):
     template_name = "motifs/motif_list.html"
     filterset_class = MotifFilter
     paginate_by = 10
+
+class MotifDeleteView(TeacherRequiredMixin, DeleteView):
+    model = Motif
+    success_url = reverse_lazy("motifs:list_motifs")
+
+
+
