ec8e52cb172ba65241a386cc3a0d493290d02a0f
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-02-02 22:13:45 +0100

    refactor: Simple change in video model to section from another model

--- TutorApp/videos/models.py
+++ TutorApp/videos/models.py
@@ -1,42 +1,21 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-
+from TutorApp.training_calendar.models import Section
 
 
 class Video(models.Model):
     title = models.CharField(max_length=255)
     youtube_url = models.URLField()
-    section = (
-        (1, "Real numbers"),
-        (2, "Language of mathematics"),
-        (3, "Systems of equations"),
-        (4, "Functions"),
-        (5, "Linear function"),
-        (6, "Planimetry"),
-        (7, "Quadratic function"),
-        (8, "Polynomials"),
-        (9, "Measurable function"),
-        (10, "Trigonometry"),
-        (11, "Planimetry circles"),
-        (12, "Exponential and logarithmic functions"),
-        (13, "Trigonometric functions"),
-        (14, "Analytical geometry"),
-        (15, "Sequences"),
-        (16, "Differential calculus"),
-        (17, "Statistics"),
-        (18, "Probability"),
-        (19, "Stereometry"),
+    section = models.ForeignKey(
+        Section, on_delete=models.CASCADE, related_name="videos"
     )
-    type = models.IntegerField(choices=section)
-
 
     subcategory = models.CharField(max_length=255)
 
-
     class VideoLevel(models.TextChoices):
-        PRIMARY = "1", _('Primary School')
-        SECONDARY = "2", _('Secondary School')
+        PRIMARY = "1", _("Primary School")
+        SECONDARY = "2", _("Secondary School")
 
     level = models.CharField(max_length=10, choices=VideoLevel.choices)
 
@@ -48,10 +27,12 @@ class Video(models.Model):
 
 class VideoTimestamp(models.Model):
     class TimestampType(models.TextChoices):
-        EXERCISE = "1", _('Exercise')
-        TASK = "2", _('Task')
+        EXERCISE = "1", _("Exercise")
+        TASK = "2", _("Task")
 
-    video = models.ForeignKey(Video, on_delete=models.CASCADE, related_name='timestamps')
+    video = models.ForeignKey(
+        Video, on_delete=models.CASCADE, related_name="timestamps"
+    )
     label = models.CharField(max_length=255)
     start_time = models.DurationField(help_text=_("Format: HH:MM:SS"))
     type = models.CharField(max_length=10, choices=TimestampType.choices)
