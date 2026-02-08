41aca642aefc16e239e36c92dd77fa20035c181c
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-02-08 20:40:07 +0100

    feat: Added add video url

--- TutorApp/videos/urls.py
+++ TutorApp/videos/urls.py
@@ -1,20 +1,8 @@
 from django.urls import path
-
-from .views import SectionListView, SubcategoryListView, VideoCreateView, VideoListView
+from videos.views.video_create_view import VideoCreateView
 
 app_name = "videos"
 
 urlpatterns = [
     path("add/", VideoCreateView.as_view(), name="add"),
-    path("", SectionListView.as_view(), name="section_list"),
-    path(
-        "section/<int:section_id>/",
-        SubcategoryListView.as_view(),
-        name="subcategory_list",
-    ),
-    path(
-        "section/<int:section_id>/<str:subcategory>/",
-        VideoListView.as_view(),
-        name="video_list",
-    ),
 ]
