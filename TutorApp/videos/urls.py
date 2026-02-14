1bb635608198c8f7fe05b7daccf61c76c7f66cf7
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-02-14 11:56:08 +0100

    feat: Added urls about delete and list of videos

--- TutorApp/videos/urls.py
+++ TutorApp/videos/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from videos.views.video_create_view import VideoCreateView
+from videos.views.video_views import VideoCreateView, VideoDeleteView, VideoListView
 
 app_name = "videos"
 
 urlpatterns = [
+    path("", VideoListView.as_view(), name="video_list"),
     path("add/", VideoCreateView.as_view(), name="add"),
+    path("delete/<int:pk>/", VideoDeleteView.as_view(), name="delete"),
 ]
