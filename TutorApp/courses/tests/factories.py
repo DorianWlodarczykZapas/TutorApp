64a7e1e20925cc0c5d01bf09d780e890a175e60f
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-02-19 20:36:14 +0100

    refactor: Removed factories about book topic and section into courses

---
+++ TutorApp/courses/tests/factories.py
@@ -0,0 +1,75 @@
+import factory
+from courses.models import Book, Section, Topic
+from examination_tasks.choices import SchoolLevelChoices
+from factory.django import DjangoModelFactory
+
+
+class BookFactory(DjangoModelFactory):
+    class Meta:
+        model = Book
+
+    title = factory.Faker("sentence", nb_words=3)
+    author = factory.Faker("name", locale="pl_PL")
+    publication_year = factory.Faker("random_int", min=2015, max=2024)
+    school_level = SchoolLevelChoices.PRIMARY
+    subject = 1
+    grade = 1
+
+    class Params:
+
+        primary_school = factory.Trait(
+            school_level=SchoolLevelChoices.PRIMARY,
+            grade=factory.Faker("random_element", elements=[7, 8]),
+        )
+
+        high_school = factory.Trait(
+            school_level=SchoolLevelChoices.choices,
+            grade=factory.Faker("random_element", elements=[1, 2, 3, 4]),
+        )
+
+        multi_grade = factory.Trait(grade=None)
+
+        no_author = factory.Trait(author="", publication_year=None)
+
+
+class SectionFactory(DjangoModelFactory):
+    class Meta:
+        model = Section
+
+    book = factory.SubFactory(BookFactory)
+
+    name = factory.Faker(
+        "random_element",
+        elements=[
+            "Linear Equations",
+            "Quadratic Equations",
+            "Thermodynamics",
+            "Kinematics",
+            "Organic Chemistry",
+            "Cell Biology",
+            "World War II",
+            "Renaissance",
+            "Grammar Basics",
+            "Reading Comprehension",
+        ],
+    )
+
+
+class TopicFactory(DjangoModelFactory):
+    class Meta:
+        model = Topic
+
+    section = factory.SubFactory(SectionFactory)
+
+    name = factory.Faker(
+        "random_element",
+        elements=[
+            "Conditional probability",
+            "Vietes formulas",
+            "Bernoullis diagram",
+            "Binomial distribution",
+            "Quadratic equations - delta",
+            "Linear functions - graph",
+            "Pythagorean theorem",
+        ],
+    )
--- TutorApp/examination_tasks/tests/factories.py
+++ TutorApp/examination_tasks/tests/factories.py
@@ -1,4 +1,5 @@
 import factory
+from courses.factories import SectionFactory, TopicFactory
 from django.contrib.auth import get_user_model
 from factory.django import DjangoModelFactory
 
@@ -7,10 +8,9 @@ from ..choices import (
     MONTH_CHOICES,
     YEAR_CHOICES,
     ExamTypeChoices,
-    SchoolLevelChoices,
     SubjectChoices,
 )
-from ..models import Book, Exam, ExamTask, Section, Topic
+from ..models import Exam, ExamTask
 
 
 class ExamFactory(DjangoModelFactory):
@@ -47,77 +47,6 @@ class ExamFactory(DjangoModelFactory):
                 "random_element", elements=[choice[0] for choice in LEVEL_CHOICES]
             ).evaluate(None, None, {"locale": None})
         return None
-
-
-class BookFactory(DjangoModelFactory):
-    class Meta:
-        model = Book
-
-    title = factory.Faker("sentence", nb_words=3)
-    author = factory.Faker("name", locale="pl_PL")
-    publication_year = factory.Faker("random_int", min=2015, max=2024)
-    school_level = SchoolLevelChoices.PRIMARY
-    subject = 1
-    grade = 1
-
-    class Params:
-
-        primary_school = factory.Trait(
-            school_level=SchoolLevelChoices.PRIMARY,
-            grade=factory.Faker("random_element", elements=[7, 8]),
-        )
-
-        high_school = factory.Trait(
-            school_level=SchoolLevelChoices.choices,
-            grade=factory.Faker("random_element", elements=[1, 2, 3, 4]),
-        )
-
-        multi_grade = factory.Trait(grade=None)
-
-        no_author = factory.Trait(author="", publication_year=None)
-
-
-class SectionFactory(DjangoModelFactory):
-    class Meta:
-        model = Section
-
-    book = factory.SubFactory(BookFactory)
-
-    name = factory.Faker(
-        "random_element",
-        elements=[
-            "Linear Equations",
-            "Quadratic Equations",
-            "Thermodynamics",
-            "Kinematics",
-            "Organic Chemistry",
-            "Cell Biology",
-            "World War II",
-            "Renaissance",
-            "Grammar Basics",
-            "Reading Comprehension",
-        ],
-    )
-
-
-class TopicFactory(DjangoModelFactory):
-    class Meta:
-        model = Topic
-
-    section = factory.SubFactory(SectionFactory)
-
-    name = factory.Faker(
-        "random_element",
-        elements=[
-            "Conditional probability",
-            "Vietes formulas",
-            "Bernoullis diagram",
-            "Binomial distribution",
-            "Quadratic equations - delta",
-            "Linear functions - graph",
-            "Pythagorean theorem",
-        ],
-    )
 
 
 class ExamTaskFactory(DjangoModelFactory):
