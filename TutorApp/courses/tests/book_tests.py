e36026e284c0e49f8069c42338459a3d2ea99b48
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-02-20 23:59:57 +0100

    feat: Added setup parameters to book test factory

--- TutorApp/courses/tests/book_tests.py
+++ TutorApp/courses/tests/book_tests.py
@@ -1,10 +1,23 @@
 from django.test import Client, TestCase
 from django.urls import reverse
+from examination_tasks.choices import SchoolLevelChoices
+
+from TutorApp.users.factories import UserFactory
 
 
 class AddBookViewTests(TestCase):
     def setUp(self):
         self.client = Client()
+        self.url = reverse("add_book")
+        self.user = UserFactory.create()
+        self.valid.data = {
+            "title": "Math for 8 grader",
+            "author": "Jan Kowalski",
+            "publication_year": 2022,
+            "school_level": SchoolLevelChoices.PRIMARY,
+            "subject": 1,
+            "grade": 8,
+        }
 
     def test_unauthorized_access(self):
         """Test case that checks if unauthorized access is working"""
