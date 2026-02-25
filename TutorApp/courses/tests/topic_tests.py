404fc5ade2066654ce9dae6858c041fe7686c01c
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-02-25 11:03:47 +0100

    test: Added test cases about getting access to add topic page

--- TutorApp/courses/tests/topic_tests.py
+++ TutorApp/courses/tests/topic_tests.py
@@ -12,3 +12,22 @@ class AddTopicTests(TestCase):
         self.teacher = TeacherFactory.create()
         self.section = SectionFactory.create()
         self.valid_data = {"section": self.section.pk, "name": "Viete's Formulas"}
+
+    def test_unauthorized_access(self):
+        """Test case that checks if unauthorized access is working"""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
+
+    def test_can_teacher_access(self):
+        """
+        Test case that checks if teacher can access adding topic page
+        """
+        self.client.force_login(self.teacher)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_can_student_access(self):
+        """Test case that checks if student can access adding topic page"""
+        self.client.force_login(self.student)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
