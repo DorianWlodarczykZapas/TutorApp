b9b772a57c87baabd66ed9391f15415358107158
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-02-26 12:59:53 +0100

    tests: Add training tasks test class with basic tests about access

---
+++ TutorApp/courses/tests/training_tasks_tests.py
@@ -0,0 +1,38 @@
+from courses.tests.factories import SectionFactory
+from django.test import Client, TestCase
+from django.urls import reverse
+from users.factories import TeacherFactory, UserFactory
+
+
+class AddTrainingTasksTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.student = UserFactory.create()
+        self.teacher = TeacherFactory.create()
+        self.url = reverse("add_training_tasks")
+        self.section = SectionFactory.create()
+        self.valid_data = {
+            "task_content": "Solve this equation",
+            "answer": "4",
+            "level": 3,
+            "section": self.section.pk,
+        }
+
+    def test_unauthorized_access(self):
+        """Test case that checks if unauthorized access is working"""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
+
+    def test_can_teacher_access(self):
+        """
+        Test case that checks if teacher can access adding topic page
+        """
+        self.client.force_login(self.teacher)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_can_student_access(self):
+        """Test case that checks if student can access adding topic page"""
+        self.client.force_login(self.student)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
