ff261b46eeca9bd1033b23f93dacb205f7a17728
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-03-03 22:04:40 +0100

    feat: Implement youtube player frame function

---
+++ TutorApp/static/assets/js/youtube_player.js
@@ -0,0 +1,22 @@
+const videoId = JSON.parse(document.getElementById('video-id').textContent);
+
+var tag = document.createElement('script')
+tag.src = "https://www.youtube.com/iframe_api";
+document.head.appendChild(tag);
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '100%',
+        width: '100%',
+        videoId: videoId,
+        playerVars: {
+            controls: 0,
+            disablekb: 1,
+            modestbranding: 1,
+            rel: 0,
+            fs: 0,
+        },
+    });
+}
