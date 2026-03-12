97e4bae9bba29d98e64ce8ae13722edc84e03993
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-03-12 13:53:05 +0100

    refactor: Expand the logic behind adding section form

--- TutorApp/courses/forms/section_forms.py
+++ TutorApp/courses/forms/section_forms.py
@@ -1,13 +1,24 @@
-from courses.models import Section
-from django.forms import forms
+from courses.models import Book, Section
+from django import forms
 from django.utils.translation import gettext_lazy as _
+from django_select2.forms import ModelSelect2MultipleWidget
 
 
-class SectionForm(forms.Form):
+class SectionForm(forms.ModelForm):
     class Meta:
         model = Section
-        fields = ["book", "name"]
+        fields = ["books", "name"]
         labels = {
-            "book": _("Book Title"),
+            "books": _("Books"),
             "name": _("Section Name"),
+        }
+        widgets = {
+            "books": ModelSelect2MultipleWidget(
+                model=Book,
+                search_fields=["title__icontains"],
+                attrs={
+                    "placeholder": _("Search for a book..."),
+                    "data-ajax--url": "/select2/fields/auto.json",
+                },
+            )
         }
