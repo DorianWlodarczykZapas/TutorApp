bc560ede62701413dda8e231719ae0d83bf02071
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-03-13 22:54:58 +0100

    fix: Changes in book form due to choices changes and mixin to not DRY

--- TutorApp/courses/forms/book_forms.py
+++ TutorApp/courses/forms/book_forms.py
@@ -1,52 +1,31 @@
+from common.forms import TypedChoiceMixin
+from courses.choices import (
+    BookTypeChoices,
+    GradeChoices,
+    SchoolLevelChoices,
+    SubjectChoices,
+)
 from courses.models import Book
 from django import forms
 from django.utils.translation import gettext_lazy as _
-from examination_tasks import choices
 
 
-class BookForm(forms.ModelForm):
+class BookForm(TypedChoiceMixin, forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        school_level_choices = [("", _("School Level"))] + list(
-            choices.SchoolLevelChoices.choices
-        )
-        subject_choices = [("", _("Subject"))] + list(choices.SubjectChoices.choices)
-        grade_choices = [("", _("Grade"))] + list(choices.GradeChoices.choices)
-        book_type_choices = [("", _("Book Type"))] + list(
-            choices.BookTypeChoices.choices
+
+        self.fields["book_type"] = self._make_typed_choice(
+            BookTypeChoices, _("Book Type")
         )
 
-        self.fields["book_type"] = forms.TypedChoiceField(
-            choices=book_type_choices,
-            widget=forms.Select(attrs={"placeholder": " "}),
-            label=_("Book Type"),
-            coerce=int,
-            empty_value=None,
+        self.fields["school_level"] = self._make_typed_choice(
+            SchoolLevelChoices, _("School Level")
         )
 
-        self.fields["school_level"] = forms.TypedChoiceField(
-            choices=school_level_choices,
-            widget=forms.Select(attrs={"placeholder": " "}),
-            label=_("School Level"),
-            coerce=int,
-            empty_value=None,
-        )
-        self.fields["subject"] = forms.TypedChoiceField(
-            choices=subject_choices,
-            widget=forms.Select(attrs={"placeholder": " "}),
-            label=_("Subject"),
-            coerce=int,
-            empty_value=None,
-        )
-        self.fields["grade"] = forms.TypedChoiceField(
-            choices=grade_choices,
-            widget=forms.Select(attrs={"placeholder": " "}),
-            label=_("Grade"),
-            coerce=int,
-            empty_value=None,
-            required=False,
-        )
+        self.fields["subject"] = self._make_typed_choice(SubjectChoices, _("Subject"))
+
+        self.fields["grade"] = self._make_typed_choice(GradeChoices, _("Grade"))
 
     class Meta:
         model = Book
