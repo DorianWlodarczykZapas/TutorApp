40212123f0424eef6d10fde5cffb58c6356da0ad
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-03-13 22:55:43 +0100

    refactor: New implementation of add section due to changes in model and mixin about select parameter

--- TutorApp/courses/forms/section_forms.py
+++ TutorApp/courses/forms/section_forms.py
@@ -1,24 +1,24 @@
-from courses.models import Book, Section
+from common.forms import TypedChoiceMixin
+from courses.choices import GradeChoices, SubjectChoices
+from courses.models import Section
 from django import forms
 from django.utils.translation import gettext_lazy as _
-from django_select2.forms import ModelSelect2MultipleWidget
 
 
-class SectionForm(forms.ModelForm):
+class SectionForm(TypedChoiceMixin, forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["subject"] = self._make_typed_choice(SubjectChoices, _("Subject"))
+        self.fields["grade"] = self._make_typed_choice(GradeChoices, _("Grade"))
+
     class Meta:
         model = Section
-        fields = ["books", "name"]
+        fields = ["grade", "subject", "name"]
         labels = {
-            "books": _("Books"),
             "name": _("Section Name"),
         }
+
         widgets = {
-            "books": ModelSelect2MultipleWidget(
-                model=Book,
-                search_fields=["title__icontains"],
-                attrs={
-                    "placeholder": _("Search for a book..."),
-                    "data-ajax--url": "/select2/fields/auto.json",
-                },
-            )
+            "name": forms.TextInput(attrs={"placeholder": " "}),
         }
