c94e0aac08116589c545c1014759dfc9b079166c
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-03-13 23:04:36 +0100

    fix: Fix imports about choices in users services

--- TutorApp/users/services.py
+++ TutorApp/users/services.py
@@ -1,10 +1,10 @@
 from datetime import timedelta
 from typing import Optional
 
+from courses.choices import GradeChoices, SchoolLevelChoices
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpRequest
 from django.utils import timezone
-from examination_tasks.choices import GradeChoices, SchoolLevelChoices
 from plans.models import Plan, UserPlan
 
 from .models import User
