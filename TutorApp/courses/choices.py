54c7a7787eece57b3784e19d4409d450cc3f5a0a
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-03-13 22:54:03 +0100

    build: Divide logic about choices into courses ones

--- TutorApp/courses/choices.py
+++ TutorApp/courses/choices.py
@@ -1,35 +1,5 @@
-from datetime import datetime
-
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
-current_year = datetime.now().year
-YEAR_CHOICES = [(y, str(y)) for y in range(2002, current_year + 1)]
-
-MONTH_CHOICES = [
-    (1, _("January")),
-    (2, _("February")),
-    (3, _("March")),
-    (4, _("April")),
-    (5, _("May")),
-    (6, _("June")),
-    (7, _("July")),
-    (8, _("August")),
-    (9, _("September")),
-    (10, _("October")),
-    (11, _("November")),
-    (12, _("December")),
-]
-
-LEVEL_CHOICES = [
-    (1, _("Basic")),
-    (2, _("Extended")),
-]
-
-
-class ExamTypeChoices(models.IntegerChoices):
-    MATRICULATION = 1, _("Matriculation Exam")
-    EIGHTH_GRADE = 2, _("Eighth Grade Exam")
 
 
 class SchoolLevelChoices(models.IntegerChoices):
