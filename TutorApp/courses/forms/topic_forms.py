27f3a63c3edac74b1d47477a0fad8217ad9d6ebd
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-03-14 23:27:19 +0100

    refactor: New implementation of add topic form

--- TutorApp/courses/forms/topic_forms.py
+++ TutorApp/courses/forms/topic_forms.py
@@ -4,10 +4,20 @@ from django.utils.translation import gettext_lazy as _
 
 
 class TopicForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["section"].empty_label = _("Section Name")
+
     class Meta:
         model = Topic
         fields = ["section", "name"]
         labels = {
             "section": _("Section Name"),
-            "name": _("Specific Topic"),
+            "name": _("Input Topic Name"),
+        }
+
+        widgets = {
+            "section": forms.Select(attrs={"placeholder": " "}),
+            "name": forms.TextInput(attrs={"placeholder": " "}),
         }
