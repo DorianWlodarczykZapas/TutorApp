be366c97b12d3484d4946c30b7ff4fee0b35bd85
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-03-21 23:36:15 +0100

    refactor: New implementation of training task form

--- TutorApp/courses/forms/training_tasks_forms.py
+++ TutorApp/courses/forms/training_tasks_forms.py
@@ -1,18 +1,38 @@
-from courses.models import TrainingTask
+from courses.models import Book, Section, TrainingTask
 from django import forms
 from django.utils.translation import gettext_lazy as _
+from django_select2.forms import ModelSelect2Widget
+from videos.models import VideoTimestamp
 
 
 class TrainingTaskForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["section"].empty_label = _("Section Name")
+        self.fields["book"].empty_label = _("Book Name")
+
     class Meta:
         model = TrainingTask
-        fields = ["task_content", "answer", "image", "section", "level"]
+        fields = [
+            "task_content",
+            "answer",
+            "image",
+            "section",
+            "level",
+            "book",
+            "page_number",
+            "explanation_timestamp",
+        ]
         labels = {
             "task_content": _("Task Content"),
             "answer": _("Answer"),
             "image": _("Task Image (optional)"),
             "section": _("Section (optional)"),
             "level": _("Difficulty Level"),
+            "book": _("Book (optional)"),
+            "page_number": _("Page Number"),
+            "explanation_timestamp": _("Explanation Timestamp"),
         }
         widgets = {
             "task_content": forms.Textarea(
@@ -23,5 +43,30 @@ class TrainingTaskForm(forms.ModelForm):
             ),
             "answer": forms.TextInput(
                 attrs={"placeholder": _("Enter the correct answer...")}
+            ),
+            "section": ModelSelect2Widget(
+                model=Section,
+                search_fields=["name__icontains"],
+                attrs={
+                    "placeholder": _("Section Name"),
+                    "data-placeholder": _("Section Name"),
+                },
+            ),
+            "book": ModelSelect2Widget(
+                model=Book,
+                search_fields=["title__icontains"],
+                attrs={
+                    "placeholder": _("Book Name"),
+                    "data-placeholder": _("Book Name"),
+                },
+            ),
+            "page_number": forms.NumberInput(attrs={"placeholder": " "}),
+            "explanation_timestamp": ModelSelect2Widget(
+                model=VideoTimestamp,
+                search_fields=["label__icontains", "video__title__icontains"],
+                attrs={
+                    "placeholder": _("Explanation Timestamp"),
+                    "data-placeholder": _("Explanation Timestamp"),
+                },
             ),
         }
