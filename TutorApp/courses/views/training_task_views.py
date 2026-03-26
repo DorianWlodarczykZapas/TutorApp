0c76f43eb76a736448780ff79b32b424202ed682
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-03-26 19:34:36 +0100

    perf: Change http into json response to not reload pagee

--- TutorApp/courses/views/training_task_views.py
+++ TutorApp/courses/views/training_task_views.py
@@ -2,14 +2,16 @@ from typing import Any, Dict
 
 from courses.filters import TrainingTaskFilter
 from courses.forms.training_tasks_forms import TrainingTaskForm
-from courses.models import TrainingTask
+from courses.models import TrainingTask, UserTaskCompletion
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseRedirect
+from django.http import HttpRequest, HttpResponseRedirect, JsonResponse
 from django.urls import reverse_lazy
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView, DetailView
 from django_filters.views import FilterView
+from plans.models import UserPlan
 from users.mixins import TeacherRequiredMixin
 
 
@@ -95,22 +97,44 @@ class TrainingTaskDetailView(LoginRequiredMixin, DetailView):
     template_name = "courses/training_task_detail.html"
     context_object_name = "task"
 
-    def get_queryset(self):
-
-        return (
-            super()
-            .get_queryset()
-            .select_related("section", "section__book")
-            .prefetch_related("completed_by")
-        )
-
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        context["title"] = _("Task Details")
 
-        if self.request.user.is_authenticated:
-            context["is_completed"] = self.object.completed_by.filter(
-                id=self.request.user.id
-            ).exists()
+        try:
+            is_premium = self.request.user.userplan.is_premium_or_trial
+        except UserPlan.DoesNotExist:
+            is_premium = False
+
+        context["is_premium"] = is_premium
+
+        if self.object.explanation_timestamp:
+            context["video_id"] = (
+                self.object.explanation_timestamp.video.youtube_video_id
+            )
+            context["start_seconds"] = self.object.explanation_timestamp.start_seconds
+            context["end_seconds"] = (
+                self.object.explanation_timestamp.next_start_seconds
+            )
+
+        context["is_completed"] = UserTaskCompletion.objects.filter(
+            user=self.request.user, task=self.object
+        ).exists()
 
         return context
+
+    def post(self, request: HttpRequest, *args, **kwargs) -> JsonResponse:
+        self.object = self.get_object()
+
+        obj, created = UserTaskCompletion.objects.update_or_create(
+            user=request.user,
+            task=self.object,
+            defaults={"completed_at": timezone.now()},
+        )
+
+        return JsonResponse(
+            {
+                "status": "success",
+                "message": _("Task marked as completed!"),
+                "is_created": created,
+            }
+        )
