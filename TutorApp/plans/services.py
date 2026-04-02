ab30c8b59e5ae75b0595d724ec755bafd3dbb4f8
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-04-02 20:58:15 +0200

    feat: Create stripe service with one and subsctription payment and customer

--- TutorApp/plans/services.py
+++ TutorApp/plans/services.py
@@ -1,3 +1,7 @@
+from decimal import Decimal
+
+import stripe
+from django.conf import settings
 from django.utils import timezone
 from plans.models import Plan, UserPlan
 
@@ -19,3 +23,44 @@ class PlanService:
                 return True
             return False
         return False
+
+
+class StripeService:
+    def __init__(self, user_plan: UserPlan):
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        self.user_plan = user_plan
+
+    def get_or_create_customer(self) -> str:
+        if self.user_plan.stripe_customer_id:
+            return self.user_plan.stripe_customer_id
+        customer = stripe.Customer.create(
+            email=self.user_plan.user.email,
+            name=self.user_plan.user.username,
+        )
+
+        self.user_plan.stripe_customer_id = customer.id
+        self.user_plan.save()
+        return self.user_plan.stripe_customer_id
+
+    def create_payment_intent(self, plan: Plan) -> stripe.PaymentIntent:
+        price = Decimal(plan.price)
+        amount = int(price * 100)
+        customer_id = self.get_or_create_customer()
+        return stripe.PaymentIntent.create(
+            amount=amount,
+            currency=plan.currency,
+            customer=customer_id,
+        )
+
+    def create_subscription(self, plan: Plan) -> stripe.Subscription:
+        customer_id = self.get_or_create_customer()
+        return stripe.Subscription.create(
+            customer=customer_id,
+            items=[{"price": plan.stripe_price_id}],
+        )
+
+    def process_payment(self, plan: Plan):
+        if plan.type == Plan.PlanType.ULTIMATE:
+            return self.create_payment_intent(plan)
+        else:
+            return self.create_subscription(plan)
