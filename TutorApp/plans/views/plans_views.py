18ccb74a76b53de346ed2129b7435ffddd34e6f5
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-04-04 23:33:33 +0200

    feat: First build of payment view

--- TutorApp/plans/views/plans_views.py
+++ TutorApp/plans/views/plans_views.py
@@ -1,6 +1,11 @@
+import stripe
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.views import View
 from django.views.generic import ListView
-from plans.models import Plan
+from plans.models import Plan, UserPlan
+from plans.services import StripeService
 
 
 class PlansListView(LoginRequiredMixin, ListView):
@@ -16,3 +21,20 @@ class PlansListView(LoginRequiredMixin, ListView):
         context["current_plan"] = user_plan.plan.name
         context["valid_to"] = user_plan.valid_to
         return context
+
+
+class ProcessPaymentView(LoginRequiredMixin, View):
+    def post(self, request, plan_id):
+        plan = get_object_or_404(Plan, id=plan_id)
+        user_plan = get_object_or_404(UserPlan, user=self.request.user)
+        stripe_service = StripeService(user_plan=user_plan)
+        try:
+
+            result = stripe_service.process_payment(plan=plan)
+
+            if plan.type == Plan.PlanType.ULTIMATE:
+                return JsonResponse({"client_secret": result.client_secret})
+            else:
+                return JsonResponse({"subscription_id": result.id})
+        except stripe.error.StripeError as e:
+            return JsonResponse({"error": str(e)}, status=400)
