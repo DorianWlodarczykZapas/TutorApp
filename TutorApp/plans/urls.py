23a173eaf9b3f3aa3d78b527604f370ad209e9d6
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-04-05 22:10:13 +0200

    build: Added url about webhook

--- TutorApp/plans/urls.py
+++ TutorApp/plans/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from plans.views.plans_views import PlansListView, ProcessPaymentView
+from plans.views.plans_views import PlansListView, ProcessPaymentView, StripeWebhookView
 
 app_name = "plans"
 
 urlpatterns = [
     path("", PlansListView.as_view(), name="plans"),
     path("payment/<int:plan_id>/", ProcessPaymentView.as_view(), name="payment"),
+    path("webhook/", StripeWebhookView.as_view(), name="webhook"),
 ]
