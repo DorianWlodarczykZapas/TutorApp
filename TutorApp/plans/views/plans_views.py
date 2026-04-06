8c9e5f3637235f1f35d545c797182e3eda46bc68
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-04-06 11:59:25 +0200

    refactor: Add blik and card payment views , remove process payment view

--- TutorApp/plans/views/plans_views.py
+++ TutorApp/plans/views/plans_views.py
@@ -1,9 +1,13 @@
+import json
+
 import stripe
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
+from django.utils.decorators import method_decorator
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import ListView
 from plans.models import Plan, UserPlan
 from plans.services import PlanService, StripeService
@@ -24,14 +28,29 @@ class PlansListView(LoginRequiredMixin, ListView):
         return context
 
 
-class ProcessPaymentView(LoginRequiredMixin, View):
+class CardProcessPaymentView(LoginRequiredMixin, View):
+    def get(self, request, plan_id):
+        plan = get_object_or_404(Plan, id=plan_id)
+        return render(
+            request,
+            "plans/card_payment.html",
+            {"plan": plan, "stripe_publishable_key": settings.STRIPE_PUBLISHABLE_KEY},
+        )
+
     def post(self, request, plan_id):
+
+        data = json.loads(request.body)
+        payment_method_id = data.get("payment_method_id")
         plan = get_object_or_404(Plan, id=plan_id)
         user_plan = get_object_or_404(UserPlan, user=self.request.user)
         stripe_service = StripeService(user_plan=user_plan)
-        try:
 
-            result = stripe_service.process_payment(plan=plan)
+        try:
+            result = stripe_service.process_payment(
+                plan=plan,
+                payment_type="card",
+                payment_method_id=payment_method_id,
+            )
 
             if plan.type == Plan.PlanType.ULTIMATE:
                 return JsonResponse({"client_secret": result.client_secret})
@@ -40,15 +59,52 @@ class ProcessPaymentView(LoginRequiredMixin, View):
         except stripe.error.StripeError as e:
             return JsonResponse({"error": str(e)}, status=400)
 
+
+class BlikProcessPaymentView(LoginRequiredMixin, View):
     def get(self, request, plan_id):
         plan = get_object_or_404(Plan, id=plan_id)
         return render(
             request,
-            "plans/payment.html",
+            "plans/blik_payment.html",
+            {"plan": plan, "stripe_publishable_key": settings.STRIPE_PUBLISHABLE_KEY},
+        )
+
+    def post(self, request, plan_id):
+        data = json.loads(request.body)
+        payment_method_id = data.get("payment_method_id")
+        blik_code = data.get("blik_code")
+        plan = get_object_or_404(Plan, id=plan_id)
+        user_plan = get_object_or_404(UserPlan, user=self.request.user)
+        stripe_service = StripeService(user_plan=user_plan)
+
+        try:
+
+            result = stripe_service.process_payment(
+                plan=plan,
+                payment_method_id=payment_method_id,
+                payment_type="blik",
+                blik_code=blik_code,
+            )
+
+            if plan.type == Plan.PlanType.ULTIMATE:
+                return JsonResponse({"client_secret": result.client_secret})
+            else:
+                return JsonResponse({"subscription_id": result.id})
+        except stripe.error.StripeError as e:
+            return JsonResponse({"error": str(e)}, status=400)
+
+
+class ConfirmPlanView(LoginRequiredMixin, View):
+    def get(self, request, plan_id):
+        plan = get_object_or_404(Plan, id=plan_id)
+        return render(
+            request,
+            "plans/confirm_plan.html",
             {"plan": plan, "stripe_publishable_key": settings.STRIPE_PUBLISHABLE_KEY},
         )
 
 
+@method_decorator(csrf_exempt, name="dispatch")
 class StripeWebhookView(View):
     def post(self, request):
         payload = request.body
