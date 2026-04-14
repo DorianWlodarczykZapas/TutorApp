b475635c0000db821733811585008ce6049cb96f
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-04-14 22:24:13 +0200

    refactor: Change in quiz form

--- TutorApp/quizes/forms/quiz_forms.py
+++ TutorApp/quizes/forms/quiz_forms.py
@@ -1,12 +1,31 @@
+from courses.models import Section
 from django import forms
 from django.utils.translation import gettext_lazy as _
+from django_select2.forms import ModelSelect2Widget
 
 from ..models import Quiz
 
 
 class QuizForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["section"].empty_label = _("Section Name")
+
     class Meta:
         model = Quiz
         fields = ["title", "section"]
 
-    labels = {"title": _("Quiz Title"), "section": _("Subject Section")}
+        labels = {"title": _("Quiz Title"), "section": _("Subject Section")}
+
+        widgets = {
+            "section": ModelSelect2Widget(
+                model=Section,
+                search_fields=["name__icontains"],
+                attrs={
+                    "placeholder": _("Section Name"),
+                    "data-placeholder": _("Section Name"),
+                },
+            ),
+            "title": forms.TextInput(attrs={"placeholder": " "}),
+        }
