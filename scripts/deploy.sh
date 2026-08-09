88b4993ee003d8167ba90d325340336dbd911882
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-08-09 22:07:27 +0200

    build: Deploy prepared for new site

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,13 +15,13 @@ echo "Building new Docker images..."
 docker compose -f docker-compose.prod.yml --env-file .env build --no-cache
 
 echo "Performing rolling update..."
-docker compose -f docker-compoes.prod.yml --env-file .env up -d --force-recreate --remove-orphans
+docker compose -f docker-compose.prod.yml --env-file .env up -d --force-recreate --remove-orphans
 
 echo "Waiting for services t o be healthy..."
 sleep 15
 
 echo "Collecting static files..."
-docker compose -f docker-compoes.prod.yml --env-file exec -T web python TutorApp/manage.py collectstatic --no-input
+docker compose -f docker-compose.prod.yml --env-file exec -T web python TutorApp/manage.py collectstatic --no-input
 
 echo "Cleaning up old images..."
 docker image prune -f
