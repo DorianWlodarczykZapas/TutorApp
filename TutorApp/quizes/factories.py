6bbc9ae6dd8a4613be6b388c3e0319b3f8551474
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-08-12 21:19:31 +0200

    tests: Create quiz factory

--- TutorApp/quizes/factories.py
+++ TutorApp/quizes/factories.py
@@ -1,23 +1,12 @@
 import factory
 
-from .models import Answer, Quiz
+from TutorApp.courses.tests.factories import SectionFactory
+from TutorApp.quizes.models import Quiz
 
 
 class QuizFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Quiz
 
-    type = 1  # Real numbers
-    question = factory.Faker("sentence")
-    explanation = factory.Faker("paragraph")
-    question_picture = None
-    explanation_picture = None
-
-
-class AnswerFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Answer
-
-    quiz = factory.SubFactory(QuizFactory)
-    text = factory.Faker("word")
-    is_correct = False
+    section = factory.SubFactory(SectionFactory)
+    title = factory.Faker("sentence", nb_words=3)
