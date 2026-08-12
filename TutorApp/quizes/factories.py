c159ddeb79ad1767f3725576544a4f8683a85d5e
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-08-12 21:35:50 +0200

    tests: Create question factory

--- TutorApp/quizes/factories.py
+++ TutorApp/quizes/factories.py
@@ -1,7 +1,8 @@
 import factory
+from examination_tasks.choices import LEVEL_CHOICES
 
 from TutorApp.courses.tests.factories import SectionFactory
-from TutorApp.quizes.models import Quiz
+from TutorApp.quizes.models import Question, Quiz
 
 
 class QuizFactory(factory.django.DjangoModelFactory):
@@ -10,3 +11,15 @@ class QuizFactory(factory.django.DjangoModelFactory):
 
     section = factory.SubFactory(SectionFactory)
     title = factory.Faker("sentence", nb_words=3)
+
+
+class QuestionFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Question
+
+    text = factory.Faker("text", max_nb_chars=250)
+    quiz = factory.SubFactory(QuizFactory)
+    level_type = factory.fuzzy.FuzzyChoice(choice[0] for choice in LEVEL_CHOICES)
+    picture = None
+    explanation = factory.Faker("text", max_nb_chars=250)
+    explanation_picture = None
