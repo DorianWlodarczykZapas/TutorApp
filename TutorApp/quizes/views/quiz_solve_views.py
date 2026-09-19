2aa556edd1a5b8ffa1d5fdcba6fe9714b3bebe84
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2026-09-19 23:08:10 +0200

    docs: Change get_context_data docstring by adding steps_name dict

--- TutorApp/quizes/views/quiz_solve_views.py
+++ TutorApp/quizes/views/quiz_solve_views.py
@@ -140,12 +140,16 @@ class SolveQuizWizard(LoginRequiredMixin, SessionWizardView):
     def get_context_data(self, form: QuizStepForm, **kwargs: Any) -> Dict[str, Any]:
         """
         Overrides the method then adds the `question` object to the context dictionary and the quiz deadline in string ISO format.
+        Adds a `steps_status` dict mapping each step name to whether the user
+        has already answered it.
 
         Args:
             form: Form instance for the current step.
 
         Returns:
-            Dict[str, Any]: Context dictionary with added question object and quiz deadline in string ISO format.
+            Dict[str, Any]: Context dictionary with added question object , quiz deadline in string ISO format
+            and steps_status dict mapping each step name to whether the user
+            has already answered it.
 
         """
         context = super().get_context_data(form=form, **kwargs)
